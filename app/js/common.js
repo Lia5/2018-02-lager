c179120aec82063418ef70366b5cf204cdcd2257
Lia5 <Liaforweb@gmail.com>
2018-02-16 08:19:45 +0200

    top fix

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,9 @@
 $(function(){
-    
+    $(".menu-toggle").on('click', function() {
+      $(this).toggleClass("on");
+      $('.fix-top__main-menu').toggleClass("on");
+      $("nav ul").toggleClass('hidden');
+    });
+
+
 })
